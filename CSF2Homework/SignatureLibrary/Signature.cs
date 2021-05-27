457ae10434561096f512e0a84aa7f066dd34a151
lavagust fox <lazelldfox@gmail.com>
2021-05-26 22:00:08 -0500

    added student

---
+++ CSF2Homework/SignatureLibrary/Signature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignatureLibrary
+{
+    public class Signature
+    {   /// <summary>
+    /// Method to print header at the start of a console application
+    /// </summary>
+    /// <param name="title"> Title to display </param>
+        public static void Hseader(string title)
+        {
+            Console.Title = title;
+            Console.WriteLine("CSF2: Homework" + title );
+        }
+    }
+}
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,6 +11,9 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+           
+            Console.WriteLine(s1);
         }
     }
 }
