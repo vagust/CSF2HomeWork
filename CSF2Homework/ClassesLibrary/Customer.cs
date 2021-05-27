b6383d448a8bb90e71abc35ba3ed69b39128331d
lavagust fox <lazelldfox@gmail.com>
2021-05-27 12:12:35 -0500

    commit all

---
+++ CSF2Homework/ClassesLibrary/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+   public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Con MyProperty { get; set; }
+    }
+}
